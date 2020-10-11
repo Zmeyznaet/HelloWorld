45f3bef0f9b0929ff6cb3f9101703fab309f4d6f
Roman <72319676+Zmeyznaet@users.noreply.github.com>
2020-10-11 11:50:13 +0300

    First_HW1.4.cpp

--- First_HW.cpp
+++ First_HW.cpp
@@ -31,7 +31,7 @@ int main()
         std:: cout << "Вы ввели 0. Рассчитать квадратное уравнение невозможно. Попробуйте снова" << std:: endl;
     }
 
-    std:: cout << "We are Hello World!" << std:: endl;
+    std:: cout << "We  are Hello World!" << std:: endl;
     
     return 0;
 }
